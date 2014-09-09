b84d34986fc65a01c8c8ee9c44db537184c39019
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-08 21:43:22 -0500

    starting over with new rails portfolio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '03b4078e14cc01ca94343f99be5af6b322e9004b60c28963222c3d34cf50245ce83d2315735ac7bbc4f85401bf6f8cc13ac25767043770bd68b11fe5c62c3e14'
