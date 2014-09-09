331f2fe5f7f5a27c6336998597f0a9a3ed354f1d
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-08 23:23:03 -0500

    added Link model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140909030119) do
+ActiveRecord::Schema.define(:version => 20140909042224) do
+
+  create_table "links", :force => true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "name"
