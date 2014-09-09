e0719292ab6710dcd34580ee1761654052a033d9
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-09 10:52:34 -0500

    added edit and update methods/routes to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,4 +22,17 @@ class ProjectsController < ApplicationController
      @project = Project.find(params[:id])
   end
   
+  def edit
+    @project = Project.find(params[:id])
+  end
+  
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      redirect_to projects_path
+    else
+      render "edit"
+    end
+  end
+  
 end
