79d21873c23710e0cc2be9085ad516d46de20589
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-09 12:02:18 -0500

    added a ton of link stuff

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,37 @@
 class LinksController < ApplicationController
+  
+  def index
+    @links = Links.all
+  end
+  
+  def new
+    @link = Link.new
+  end
+  
+  def create
+    @link = Link.new(params[:link])
+
+    if @link.save
+      redirect_to links_path
+    else
+      render "new"
+    end
+  end
+  
   def show
-    @name = "Katie"
-    @url = ""
+     @link = Link.find(params[:id])
+  end
+  
+  def edit
+     @link = Link.find(params[:id])
+  end
+  
+  def update
+    @link = Link.find(params[:id])
+    if @link.update_attributes(params[:link])
+      redirect_to links_path
+    else
+      render "edit"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,12 @@ Portfolio::Application.routes.draw do
   get "contacts/:id" => 'contacts#show', :as =>"contact"
   get "contacts/:id/edit" => 'contacts#edit', :as =>"edit_contact"
   put "contacts/:id" => 'contacts#update'
+  
+  get "links/index" => 'links#index', :as =>"links"
+  get "links/new" => 'links#new', :as =>"new_link"
+  post "links/index" => 'links#create'
+  get "links/:id" => 'links#show', :as =>"links"
+  get "links/:id/edit" => 'links#edit', :as =>"edit_link"
+  put "links/:id" => 'links#update'
 
 end
