892d39bc62f66d0cb3ee1162a2366e4498e60908
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-11 22:06:44 -0500

    added user stuff as per demo

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    
+    if @user && @user.save
+      redirect root_path
+    else
+      raise "Invalid.  Please try again."
+    end
+  end
+end
