b434da18bf2f839d5bfa3cfca8920db5cdee2145
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-12-01 12:08:10 -0600

    fixed the mess of projects with articulate, accurate info and new improved photos!

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :github, :lesson, :name, :status, :summary
+  attr_accessible :github, :lesson, :name, :status, :summary, :photo
   
   validates :name, presence: true
   mount_uploader :photo, PhotoUploader
